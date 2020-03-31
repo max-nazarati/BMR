b865df25a31a05a1a243c7cb6ac7fb3af717f283
nazarati <nazarati_maximilian@protonmail.com>
2020-03-31 16:09:45 +0200

    pushing project structure

---
+++ basicmovierecommender/src/main/java/com/nazarati/basicmovierecommender/BasicmovierecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.nazarati.basicmovierecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicmovierecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicmovierecommenderApplication.class, args);
+	}
+
+}
