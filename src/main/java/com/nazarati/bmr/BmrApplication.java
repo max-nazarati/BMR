c89e09d88ebae225f52ec96c68088cce97b02273
nazarati <nazarati_maximilian@protonmail.com>
2020-04-04 18:30:49 +0200

    initial heroku commit

---
+++ src/main/java/com/nazarati/bmr/BmrApplication.java
@@ -0,0 +1,13 @@
+package com.nazarati.bmr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BmrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BmrApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/nazarati/bmr/secretystuff/APISecrets.java
@@ -0,0 +1,7 @@
+package com.nazarati.bmr.secretystuff;
+
+public abstract class APISecrets {
+	public static String apiKey() {
+		return "api_key=e76898e95dfb9f1092951cf48f2b9eb2";
+	}
+}
